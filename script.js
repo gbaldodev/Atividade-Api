161845508190d27402efeb6634fe0097c5211f8c
Giovanni Baldo <giibola21@gmail.com>
2025-07-22 23:11:39 -0300

    Add jscript e webstorage

--- script.js
+++ script.js
@@ -1,1 +1,34 @@
-document.getElementById("entradaCep").eve
+document.getElementById("entradaCep").addEventListener("blur", (evento) => {
+  const elemento = evento.target;
+  const cepInformado = elemento.value;
+
+  if (!(cepInformado.length === 8))
+    return alert("erro");
+
+  fetch(`https://viacep.com.br/ws/${cepInformado}/json/`)
+    .then(response => response.json())
+    .then(data => {
+      if (!data.erro) {
+        document.getElementById('logradouro').value = data.logradouro;
+        document.getElementById('bairro').value = data.bairro;
+        document.getElementById('cidade').value = data.localidade;
+        document.getElementById('estado').value = data.uf; // corrigido: era 'data.estado'
+        document.getElementById('ddd').value = data.ddd;
+
+        const salvando = {
+          logradouro: data.logradouro,
+          bairro: data.bairro,
+          cidade: data.localidade,
+          estado: data.uf,
+          ddd: data.ddd
+        };
+
+        localStorage.setItem("enderecoSalvo", JSON.stringify(salvando));
+        console.log(salvando);
+
+      } else {
+        alert("cep not found");
+      }
+    })
+    .catch(error => alert("erro ao buscar", error));
+});
