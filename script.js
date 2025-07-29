1674c2ceca020cb2635f67ce5f4b5045d9330e71
Giovanni Baldo <giibola21@gmail.com>
2025-07-28 23:50:20 -0300

    Refatorar: ajustar estrutura HTML e corrigir estilos CSS

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ document.getElementById("entradaCep").addEventListener("blur", (evento) => {
         document.getElementById('logradouro').value = data.logradouro;
         document.getElementById('bairro').value = data.bairro;
         document.getElementById('cidade').value = data.localidade;
-        document.getElementById('estado').value = data.uf; // corrigido: era 'data.estado'
+        document.getElementById('estado').value = data.uf; 
         document.getElementById('ddd').value = data.ddd;
 
         const salvando = {
@@ -30,5 +30,18 @@ document.getElementById("entradaCep").addEventListener("blur", (evento) => {
         alert("cep not found");
       }
     })
-    .catch(error => alert("erro ao buscar", error));
+    .catch(error => alert("erro ao buscar", error))
+
 });
+ window.addEventListener("load", () => {
+      const resetDados = localStorage.getItem("enderecoSalvo");
+      if (resetDados){
+        const endereco = JSON.parse(resetDados);
+        document.getElementById('logradouro').value = endereco.logradouro;
+        document.getElementById('bairro').value = endereco.bairro;
+        document.getElementById('cidade').value = endereco.cidade;
+        document.getElementById('estado').value = endereco.estado; 
+        document.getElementById('ddd').value = endereco.ddd;
+      }
+  });
+  
